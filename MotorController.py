2fe3a4fd1750a6f166c6631c030be7c0c200b12c
Mason Glenn <mdglenn@go.olemiss.edu>
2021-10-29 12:41:07 -0500

    Corrected Motor Controller Code

--- MotorController.py
+++ MotorController.py
@@ -1,39 +1,43 @@
 from time import sleep
 import RPi.GPIO as GPIO
 
-DIR = 20  # Direction GPIO Pin
-STEP = 21 # Step GPIO Pin
-CW = 1    # Clockwise Rotation
-CCW = 0   # Counterclockwise Rotation
-SPR = 48  # Steps per Revolution (360/7.5)
+DIR = 20   # Direction GPIO Pin
+STEP = 21  # Step GPIO Pin
+CW = 0     # Clockwise Rotation
+CCW = 1    # Counterclockwise Rotation
+SPR = 48   # Steps per Revolution (360 / 7.5)
 
-MODE = (14, 15, 18)
-GPIO.setup(MODE, GPIO.OUT)
-RESOLUTION = {'Full' : (0, 0, 0),
-	      'Half' : (1, 0, 0),
-	      '1/4'  : (0, 1, 0),
-	      '1/8'  : (1, 1, 0),
-	      '1/16' : (0, 0, 1),
-	      '1/32' : (1, 0, 1)}
-GPIO.output(MODE, RESOLUTION['Full'])
-
+GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(DIR, GPIO.OUT)
 GPIO.setup(STEP, GPIO.OUT)
-GPIO.setup(DIR, CW)
 
 step_count = SPR
 delay = .0208
 
-for x in range(step_count):
-	GPIO.output(STEP, GPIO.HIGH)
-	sleep(delay)
-	GPIO.output(STEP, GPIO.LOW)
-	sleep(delay)
+check = int(input("press 1 for clockwise 2 for counterclockwise: "))
 
-slee(.5)
-GPIO.output(DIR, CCW)
-	GPIO.output(STEP, GPIO.HIGH)
-	sleep(delay)
-	GPIO.output(STEP, GPIO.LOW)
-	sleep(delay)
+while(1):
+	if check == 1:
+		GPIO.output(DIR, CW)
+		for x in range(step_count):
+			GPIO.output(STEP, GPIO.HIGH)
+			sleep(delay)
+			GPIO.output(STEP, GPIO.LOW)
+			sleep(delay)
+		check = int(input('press 1 for clockwise 2 for counterclockwise: '))
+	
+	elif check == 2:
+		GPIO.output(DIR, CCW)
+		for x in range(step_count):
+			GPIO.output(STEP, GPIO.HIGH)
+			sleep(delay)
+			GPIO.output(STEP, GPIO.LOW)
+			sleep(delay)
+		check = int(input('press 1 for clockwise 2 for counterclockwise: '))
+	else:
+		check = int(input('Error: incorrect input. 1 for CW 2 for CCW: '))
+		
+	
+
+GPIO.cleanup()
