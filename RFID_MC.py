d0e8cf7eb3073931120c82376d81352a88009443
Mason Glenn <mdglenn@go.olemiss.edu>
2022-03-11 07:57:14 -1000

    The actual last addition to RFID_MC.py hopefully

--- RFID_MC.py
+++ RFID_MC.py
@@ -7,33 +7,33 @@ reader = SimpleMFRC522()
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(15, GPIO.OUT, initial = GPIO.LOW) #Counterclockwise -- Open
 GPIO.setup(13, GPIO.OUT, initial = GPIO.LOW) #Clockwise -- Close
-
-check = ''
+GPIO.setup(31, GPIO.OUT, initial = GPIO.LOW) #Red -- Closed
+GPIO.setup(33, GPIO.OUT, initial = GPIO.LOW) #Green -- Opened
 
 again = 'Y'
 
-while(check == ''):
-    id, check = reader.read()
-
 while(again == 'Y'):
     sleep(.3)
+    id, check = reader.read()
     check = check.strip(' ')
     if(check == 'Open'):
         GPIO.output(13, GPIO.HIGH)
+        GPIO.output(33, GPIO.LOW)
         print('closing')
         sleep(5)
         GPIO.output(13, GPIO.LOW)
         reader.write('Closed')
+        GPIO.output(31, GPIO.HIGH)
         print('Closed')
-        id, check = reader.read()
         again = input('Would you like another iteration? Y/N: ')
     elif(check == 'Closed'):
         GPIO.output(15, GPIO.HIGH)
+        GPIO.output(31, GPIO.LOW)
         print('opening')
         sleep(5)
         GPIO.output(15, GPIO.LOW)
         reader.write('Open')
+        GPIO.output(33, GPIO.HIGH)
         print('Open')
-        id, check = reader.read()
         again = input('Would you like another iteration? Y/N: ')
 GPIO.cleanup()
